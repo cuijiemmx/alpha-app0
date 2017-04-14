bb1624e105292206c910b74b8bc24708e0e6ebe9
casa <cuijiemmx@163.com>
2017-04-15 02:51:57 +0800

    fix bugs

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
 	if (token) {
 		// console.log(`http://localhost:3000/account?access_token=${token.access_token}`)
 		rp({
-			url: `http://localhost:3000/account`,
+			url: `http://edu.updust.com/account`,
 			qs: {
 				access_token: token.access_token
 			},
@@ -77,7 +77,11 @@ app.get('/', (req, res) => {
 		}).then(function(r) {
 			var username = r.username
 			res.render('index', {username: username})
+		}).catch(function () {
+			res.render('index')
 		})
+	} else {
+		res.render('index')
 	}
 
 });
